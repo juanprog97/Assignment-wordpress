4b3ff30b70550c5c7df5e7f7db0ee796f99273aa
Juan Miguel Cardona A <juancard190@gmail.com>
2024-05-28 23:58:54 -0500

    Added new info

--- api-wordpress/src/index.js
+++ api-wordpress/src/index.js
@@ -14,16 +14,19 @@ app.get("/pricing-details", (req, res) => {
   res.json({
     data: [
       {
+        id: "free-subscription-commerce",
         title: "Free Trial",
         body: "Experience the basics of Wonderful Services at no cost with our Free Trial subscription. This package allows you to explore our core features and get a taste of the quality and convenience we provide. Perfect for new users who want to test the waters before committing.",
         price: "Free",
       },
       {
+        id: "monthly-subscription-commerce",
         title: "Monthly Subscription",
         body: "Our Monthly Subscription is ideal for those who prefer flexibility. Enjoy full access to all our services and exclusive benefits without a long-term commitment. With this plan, you can enjoy month-to-month convenience and cancel anytime if your needs change.",
         price: "500$",
       },
       {
+        id: "yearly-subscription-commerce",
         title: "Yearly Subscription",
         body: "For the best value and maximum benefits, choose our Yearly Subscription. This package offers everything in the Monthly plan but at a discounted annual rate. It’s perfect for loyal customers who want to enjoy uninterrupted access to our premium features and exclusive perks all year long.",
         price: "2500$",
